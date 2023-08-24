0d8c6f7fb61f3813d7e35fc0806283256f74fd52
EdLitvinov <edoss@list.ru>
2023-08-24 14:23:34 +0400

    example_021a

---
+++ example_021a.c
@@ -0,0 +1,18 @@
+// Вычисление НОД двух чисел первый способ
+#include <stdio.h>
+
+int main() {
+    int n1, n2, i, gcd;
+
+    printf("Введите два целых числа: \n");
+    scanf("%d %d", &n1, &n2);
+
+    for (i = 1; i <= n1 && i <= n2; ++i) {
+        if (n1 % i == 0 && n2 % i == 0)
+            gcd = i;
+    }
+
+    printf("НОД чисел: %d %d - %d\n", n1, n2, gcd);
+
+    return 0;
+}
