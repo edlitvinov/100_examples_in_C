fe9828890fd4efe2371580eef087ee3181095a4e
EdLitvinov <edoss@list.ru>
2023-09-17 15:41:20 +0400

    example_031

---
+++ example_031.c
@@ -0,0 +1,19 @@
+// пирамида из *
+# include <stdio.h>
+
+int main() {
+    int i, j, rows;
+
+    printf("Введите число (кол-во строк): \n");
+    scanf("%d", &rows);
+
+    for (i = 1; i <= rows; ++i) {
+        for (j = 1; j <= i; ++j) {
+            printf("* ");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
